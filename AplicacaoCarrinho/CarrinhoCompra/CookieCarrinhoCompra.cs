924c4d7c60febbe06c35c8e0c6e4047b107c17c5
nicolasSchinagel <drivepoweringames@gmail.com>
2026-04-07 08:49:16 -0300

    modificando Carrinho Cookie

---
+++ AplicacaoCarrinho/CarrinhoCompra/CookieCarrinhoCompra.cs
@@ -0,0 +1,18 @@
+﻿using AplicacaoCarrinho.Cookie;
+using AplicacaoCarrinho.Models;
+
+namespace AplicacaoCarrinho.CarrinhoCompra
+{
+
+    
+    public class CookieCarrinhoCompra()
+    {
+        private string Key = "Carrinho.Compras";
+        private Cookie.Cookie _cookie;
+
+        public CookieCarrinhoCompra(Cookie.Cookie cookie)
+        {
+            _cookie = cookie;
+        }
+    }
+}
--- AplicacaoCarrinho/Cookie/Cookie.cs
+++ AplicacaoCarrinho/Cookie/Cookie.cs
@@ -10,5 +10,41 @@
             _configuration = configuration;
         }
         // parei aqui 
+
+        public void Cadastrar(string Key, string Valor)
+        {
+            CookieOptions Options = new CookieOptions();
+            Options.Expires = DateTime.Now.AddDays(7);
+            Options.IsEssential = true;
+
+            _context.HttpContext.Response.Cookies.Append(Key, Valor, Options);
+        }
+        public void Atualizar(string Key, string Valor)
+        {
+            if(Existe(Key))
+            {
+                Remover(Key);
+            }
+            Cadastrar(Key, Valor);
+        }
+        public void Remover(string Key)
+        {
+            var valor = _context.HttpContext.Request.Cookies[Key];
+        }
+        public string Consultar(string Key, bool Cript = true)
+        {
+            var valor = _context.HttpContext.Request.Cookies[Key];
+            return valor;
+        }
+        public bool Existe(string Key)
+        {
+            if (_context.HttpContext.Request.Cookies[Key] == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
     }
 }
